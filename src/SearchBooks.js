5b77ae431a87981bee1076c20727c8b7af70a2fe
Aditya Mehra <mehraaditya71@gmail.com>
2017-07-25 00:21:41 -0700

    Added new line to search terms

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -79,8 +79,7 @@ class SearchBooks extends Component {
                 )
             }
             { !Array.isArray(this.state.showingBooks) &&
-                (
-                 <div>
+                (<div>
                    <h2> Try searching with these terms:</h2>
                    <p>
                      'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'History', 'History', 'Homer', 'Horror', 'Hugo,
